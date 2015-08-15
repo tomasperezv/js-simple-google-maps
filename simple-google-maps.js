4ae7b7a4db6e7af4501ac4dda2839393627ba62a
Tomas Perez <tom@0x101.com>
2015-08-15 19:05:33 +0200

    Applying jslint.

--- simple-google-maps.js
+++ simple-google-maps.js
@@ -10,6 +10,9 @@
  * No warranty expressed or implied. Use at your own risk.
  */
 
+/*jslint browser: true*/
+/*global google, SimpleGoogleMaps */
+
 /**
  * @namespace SimpleGoogleMaps
  */
@@ -21,140 +24,140 @@ window.SimpleGoogleMaps = window.SimpleGoogleMaps || {};
  */
 SimpleGoogleMaps.Map = function(options) {
 
-	this._map = null;
-	this._geoCoder = null;
+  this._map = null;
+  this._geoCoder = null;
 
-	this.DEFAULT_ZOOM = 5;
-	this.DEFAULT_MAP = google.maps.MapTypeId.ROADMAP;
+  this.DEFAULT_ZOOM = 5;
+  this.DEFAULT_MAP = google.maps.MapTypeId.ROADMAP;
 
-	// Initialize the options
-	this._options = options || {};
+  // Initialize the options
+  this._options = options || {};
 
-	// Default values
-	if (typeof this._options.zoom === 'undefined') {
-		this._options.zoom = this.DEFAULT_ZOOM;
-	}
+  // Default values
+  if (typeof this._options.zoom === 'undefined') {
+    this._options.zoom = this.DEFAULT_ZOOM;
+  }
 
-	if (typeof this._options.mapType === 'undefined') {
-		this._options.mapType = this.DEFAULT_MAP;
-	}
+  if (typeof this._options.mapType === 'undefined') {
+    this._options.mapType = this.DEFAULT_MAP;
+  }
 
-	// If we are passing the HTMLElement, then we can
-	// render directly the map
-	if (typeof this._options.div !== 'undefined') {
-		this.render(this._options.div);
-	}
+  // If we are passing the HTMLElement, then we can
+  // render directly the map
+  if (typeof this._options.div !== 'undefined') {
+    this.render(this._options.div);
+  }
 
-}
+};
 
 SimpleGoogleMaps.Map.prototype = {
 
-	/**
-	 * @param {HTMLElement} element
-	 */
-	render: function(element) {
-		if (!this.hasMap()) {
-			this._map = new google.maps.Map(element, {
-				zoom: this._options.zoom,
-				mapTypeId: this._options.mapType
-			});
-		}
-	},
+  /**
+   * @param {HTMLElement} element
+   */
+  render: function(element) {
+    if (!this.hasMap()) {
+      this._map = new google.maps.Map(element, {
+        zoom: this._options.zoom,
+        mapTypeId: this._options.mapType
+      });
+    }
+  },
 
-	/**
-	 * Centers the map based on the address.
-	 * @param {String} address e.g. 'Madrid'
-	 */
-	center: function(address) {
-		if (this.hasMap()) {
-			var self = this;
-			this.searchByAddress(address, function(location) {
-				self._map.setCenter(location);
-			});
-		}
-	},
+  /**
+   * Centers the map based on the address.
+   * @param {String} address e.g. 'Madrid'
+   */
+  center: function(address) {
+    if (this.hasMap()) {
+      var self = this;
+      this.searchByAddress(address, function(location) {
+        self._map.setCenter(location);
+      });
+    }
+  },
 
-	/**
-	 * It only returns the first result.
-	 * @param {String} address
-	 * @param {Function} callback
-	 */
-	searchByAddress: function(address, callback) {
-		var geocoder = this.getGeoCoder();
-		geocoder.geocode( { 'address': address}, function(results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
-				callback(results[0].geometry.location);
-			}
-		});
-	},
+  /**
+   * It only returns the first result.
+   * @param {String} address
+   * @param {Function} callback
+   */
+  searchByAddress: function(address, callback) {
+    var geocoder = this.getGeoCoder();
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        callback(results[0].geometry.location);
+      }
+    });
+  },
 
-	/**
-	 * @return {Boolean} hasMap True if a map has been initialized.
-	 */
-	hasMap: function() {
-		return this._map !== null;
-	},
+  /**
+   * @return {Boolean} hasMap True if a map has been initialized.
+   */
+  hasMap: function() {
+    return this._map !== null;
+  },
 
-	/**
-	 * @return {Geocoder} geocoder
-	 */
-	getGeoCoder: function() {
-		if (this._geoCoder === null) {
-			this._geoCoder = new google.maps.Geocoder();
-		}
-		return this._geoCoder;
-	},
+  /**
+   * @return {Geocoder} geocoder
+   */
+  getGeoCoder: function() {
+    if (this._geoCoder === null) {
+      this._geoCoder = new google.maps.Geocoder();
+    }
+    return this._geoCoder;
+  },
 
-	/**
-	 * @param {LatLng} position
-	 * @param {Boolean} visible
-	 * @return {Marker} marker
-	 */
-	addMarker: function(position, visible) {
-		var marker = null;
-		if (this.hasMap()) {
-			var visible = visible || true;
-			marker = new google.maps.Marker({
-				position: position,
-				map: this._map,
-				visible: visible
-			});
-		}
-		return marker;
-	},
+  /**
+   * @param {LatLng} position
+   * @param {Boolean} visible
+   * @return {Marker} marker
+   */
+  addMarker: function(position, visible) {
+    var marker = null;
+    if (this.hasMap()) {
+      visible = visible || true;
+      marker = new google.maps.Marker({
+        position: position,
+        map: this._map,
+        visible: visible
+      });
+    }
+    return marker;
+  },
 
-	/**
-	 * @param {Object} position
-	 * @param {String} text
-	 * @param {String} id
-	 * @return {Label} label
-	 */
-	addLabel: function(position, text, id) {
-		var label = null;
-		if (this.hasMap()) {
-			label = new SimpleGoogleMaps.Label({
-				map: this._map,
-				position: position,
-				text: text,
-				id: id
-			});
-		}
-		return label;
-	},
+  /**
+   * @param {Object} position
+   * @param {String} text
+   * @param {String} id
+   * @return {Label} label
+   */
+  addLabel: function(position, text, id) {
+    var label = null;
+    if (this.hasMap()) {
+      label = new SimpleGoogleMaps.Label({
+        map: this._map,
+        position: position,
+        text: text,
+        id: id
+      });
+    }
+    return label;
+  },
 
-	/**
-	 * @param {Float} lat
-	 * @param {Float} lng
-	 * @return {LatLng} position
-	 */
-	getPosition: function(lat, lng) {
-		return new google.maps.LatLng(lat, lng);
-	},
+  /**
+   * @param {Float} lat
+   * @param {Float} lng
+   * @return {LatLng} position
+   */
+  getPosition: function(lat, lng) {
+    return new google.maps.LatLng(lat, lng);
+  },
 
-	/**
-	 * @return {google.maps.Map}
-	 */
-	getGoogleMap: function() {
-		return this._map;
-	}
+  /**
+   * @return {google.maps.Map}
+   */
+  getGoogleMap: function() {
+    return this._map;
+  }
 };
